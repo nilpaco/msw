7941ed3e34c117c0ab7d1323816002811057ca2b
Nil Panés Coll <nil@nuvolar.eu>
2018-07-22 11:10:53 +0200

    Tslint mod and logo

--- src/app/componentes/spot/spot.component.ts
+++ src/app/componentes/spot/spot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotService } from '../../services/spot.service';
 
 @Component({
 	selector: 'app-spot',
@@ -9,13 +10,17 @@ export class SpotComponent implements OnInit {
 	public spots: any[] = [];
 	public loading = true;
 
-	constructor() {}
+	constructor(private spotService: SpotService) {}
 
 	ngOnInit() {
-		setTimeout(() => {
-			this.spots = [ 1, 2, 3 ];
+		// setTimeout(() => {
+		// 	this.spots = [ 1, 2, 3 ];
 
+		// 	this.loading = false;
+		// }, 2000);
+		this.spotService.getUsers().subscribe((res) => {
 			this.loading = false;
-		}, 2000);
+			this.spots = res;
+		});
 	}
 }
